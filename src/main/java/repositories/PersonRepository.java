526c8fbe4cd4f3e4b713dbf3985d90ff6f641713
niko-air <nicolasferreyra19@gmail.com>
2018-10-30 23:33:24 -0300

    add spring boot application

--- src/main/java/repositories/PersonRepository.java
+++ src/main/java/repositories/PersonRepository.java
@@ -3,8 +3,5 @@ package repositories;
 import model.Person;
 import org.springframework.data.repository.CrudRepository;
 
-/**
- * Created by nferreyra on 30/10/18.
- */
 public interface PersonRepository extends CrudRepository<Person, Integer> {
 }
